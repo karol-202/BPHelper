77588d4e22b7f8cf40aa19afa4077949c93ad93b
karol-202 <karoljurski1@gmail.com>
2019-01-11 14:52:48 +0100

    Initial commit.

---
+++ app/src/main/java/pl/karol202/bphelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.karol202.bphelper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

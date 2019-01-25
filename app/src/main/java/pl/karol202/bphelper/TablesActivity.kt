7c03fabe4feacc94683d6b3ad0c93e87327d1956
karol-202 <karoljurski1@gmail.com>
2019-01-25 17:21:12 +0100

    Add tables UI.

--- app/src/main/java/pl/karol202/bphelper/TablesActivity.kt
+++ app/src/main/java/pl/karol202/bphelper/TablesActivity.kt
@@ -5,13 +5,12 @@ import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
+import com.google.android.material.chip.Chip
+import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.design._CoordinatorLayout
 import org.jetbrains.anko.design.appBarLayout
 import org.jetbrains.anko.design.coordinatorLayout
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.setContentView
 
 class TablesActivity : AppCompatActivity()
 {
@@ -23,7 +22,7 @@ class TablesActivity : AppCompatActivity()
 	private lateinit var membersViewModel: MembersViewModel
 	private lateinit var tableConfiguration: TableConfiguration
 
-	private val ui = TablesActivityUI()
+	private lateinit var ui: TablesActivityUI
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -31,9 +30,7 @@ class TablesActivity : AppCompatActivity()
 		super.onCreate(savedInstanceState)
 		initViewModel()
 		initTablesConfiguration(savedInstanceState)
-
-		ui.setContentView(this)
-		initToolbar()
+		initUI()
 	}
 
 	private fun initViewModel()
@@ -53,13 +50,20 @@ class TablesActivity : AppCompatActivity()
 		return configurationType.factory.createForMembers(membersViewModel.allMembers.value ?: return null)
 	}
 
+	private fun initUI()
+	{
+		ui = TablesActivityUI(tableConfiguration)
+		ui.setContentView(this)
+		initToolbar()
+	}
+
 	private fun initToolbar()
 	{
 		setSupportActionBar(ui.toolbar)
 	}
 }
 
-private class TablesActivityUI : AnkoComponent<TablesActivity>
+private class TablesActivityUI(private val tableConfiguration: TableConfiguration) : AnkoComponent<TablesActivity>
 {
 	lateinit var toolbar: Toolbar
 
@@ -71,7 +75,38 @@ private class TablesActivityUI : AnkoComponent<TablesActivity>
 					doOnApi(Build.VERSION_CODES.LOLLIPOP) { elevation = dip(4).toFloat() }
 				}
 			}
-
+			createViewForTableConfiguration()
 		}
+	}
+
+	private fun _CoordinatorLayout.createViewForTableConfiguration() = when(tableConfiguration)
+	{
+		is TableConfiguration4X2 -> createViewForTableConfiguration4x2(tableConfiguration)
+		is TableConfiguration2X3 -> createViewForTableConfiguration2x3(tableConfiguration)
+		else -> throw Exception("Unsupported table configuration")
+	}
+
+	private fun _CoordinatorLayout.createViewForTableConfiguration4x2(tableConfiguration: TableConfiguration4X2) = gridLayout {
+		columnCount = 2
+
+		createViewForTable2(tableConfiguration.openingGov)
+		createViewForTable2(tableConfiguration.openingOpp)
+		createViewForTable2(tableConfiguration.closingGov)
+		createViewForTable2(tableConfiguration.closingOpp)
+	}
+
+	private fun _GridLayout.createViewForTable2(table: TableConfiguration4X2.Table) = verticalLayout {
+		chip
+	}
+
+	private fun _CoordinatorLayout.createViewForTableConfiguration2x3(tableConfiguration: TableConfiguration2X3) = gridLayout {
+		columnCount = 2
+
+		createViewForTable3(tableConfiguration.gov)
+		createViewForTable3(tableConfiguration.opp)
+	}
+
+	private fun _GridLayout.createViewForTable3(table: TableConfiguration2X3.Table) = verticalLayout {
+
 	}
 }
